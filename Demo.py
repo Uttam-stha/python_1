497fcdf7b077821dafc9ac517018751a6dca4c96
Uttam-stha <uttamshrestha9810@gmail.com>
2026-06-26 21:20:50 +0545

    first commit

---
+++ Demo.py
@@ -0,0 +1,7 @@
+name = "Uttam"
+age = 25
+height = 5.9
+is_student = True
+
+print(name)
+print(is_student)
---
+++ dem.py
@@ -0,0 +1,9 @@
+class Car:
+    def __init_(self, brand):
+        self.brand = brand
+
+    def drive(self):
+        print(f"{self.brand} is driving.")
+
+my_car = Car("Tesla")
+my_car.drive()
